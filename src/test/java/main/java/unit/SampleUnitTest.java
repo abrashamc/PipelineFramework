337d5089eb4638415fc7a3578cb600b02c561f53
achowdhury <abrasham.chowdhury@bamtechmedia.com>
2020-06-12 17:13:22 -0400

    adding test

---
+++ src/test/java/main/java/unit/SampleUnitTest.java
@@ -0,0 +1,14 @@
+package unit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleUnitTest {
+
+    @Test
+    public void simpleTest() {
+        Assert.assertTrue(true);
+    }
+
+
+}
